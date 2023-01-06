349d1c7f3a132fa7125c5e6eeec233219ecccfe9
codeHub <vy.code92@gmail.com>
2023-01-07 02:45:11 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 2, justifyContent: "center", alignItems: "center" }}>
+      <Text>
+        Try editing
+      </Text>
+    </View>
+  );
+}
+export default App;
